11d0db6798e89e6d90682892d891dcec58e21aa9
Yash <randomyashh@gmail.com>
2024-03-11 15:28:30 +0530

    modified/renamed: Chessboard,Tiles/SVG's

--- src/Components/Chessboard/ChessBoard.tsx
+++ src/Components/Chessboard/ChessBoard.tsx
@@ -1,5 +1,76 @@
 import Tile from "../Tiles/Tiles";
 import "./ChessBoard.css";
+
+interface Pieces {
+  peices?: string;
+  positionX: number;
+  positionY: number;
+}
+
+const pieces: Pieces[] = [];
+
+for (let i = 0; i < 8; i++) {
+  pieces.push({
+    peices: "assets/Chess SVG/blackPawn.svg",
+    positionX: i,
+    positionY: 1,
+  });
+}
+
+for (let p = 0; p < 2; p++) {
+  const color = p === 0 ? "black" : "white";
+  const positionY = p === 0 ? 0 : 7;
+
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Knight.svg`,
+    positionX: 6,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Knight.svg`,
+    positionX: 1,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}King.svg`,
+    positionX: 4,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Queen.svg`,
+    positionX: 3,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Rook.svg`,
+    positionX: 7,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Rook.svg`,
+    positionX: 0,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Bishop.svg`,
+    positionX: 2,
+    positionY,
+  });
+  pieces.push({
+    peices: `assets/Chess SVG/${color}Bishop.svg`,
+    positionX: 5,
+    positionY,
+  });
+}
+
+for (let i = 0; i < 8; i++) {
+  pieces.push({
+    peices: "assets/Chess SVG/whitePawn.svg",
+    positionX: i,
+    positionY: 6,
+  });
+}
+
 const Yaxis = ["0", "1", "2", "3", "4", "5", "6", "7"];
 const Xaxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 export default function ChessBoard() {
@@ -7,8 +78,15 @@ export default function ChessBoard() {
   for (let j = Yaxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < Xaxis.length; i++) {
       const num = i + j + 2;
+      let image = undefined;
 
-      boardChess.push(<Tile number={num} />);
+      pieces.forEach((p) => {
+        if (p.positionX === i && p.positionY === j) {
+          image = p.peices;
+        }
+      });
+
+      boardChess.push(<Tile pieces={image} number={num} />);
     }
   }
   return (
--- src/Components/Tiles/Tiles.tsx
+++ src/Components/Tiles/Tiles.tsx
@@ -1,13 +1,22 @@
 import "./Tiles.css";
 
 interface Props {
+  pieces?: string;
   number: number;
 }
 
-export default function Tile({ number }: Props) {
+export default function Tile({ number, pieces }: Props) {
   if (number % 2 === 0) {
-    return <div className="tiles greenTiles"></div>;
+    return (
+      <div className="tiles greenTiles">
+        <img src={pieces} />
+      </div>
+    );
   } else {
-    return <div className="tiles whiteTiles"></div>;
+    return (
+      <div className="tiles whiteTiles">
+        <img src={pieces} />
+      </div>
+    );
   }
 }
